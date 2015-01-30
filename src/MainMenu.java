0ea53b82941ef577e83ac981446bdda0a0b3544b
JonasArvidsson1 <jonas.arvidssson@gmail.com>
2015-01-30 11:28:29 +0100

    jonas

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -18,6 +18,9 @@ public class MainMenu extends JFrame {
 	public void HelloWorld(){
 		System.out.println("Hello world!");
 	}
+	public void Jonas(){
+		System.out.println("Jonas was here");
+	}
 	
 	public static void main(String[] args) {
 		MainMenu mainmenu = new MainMenu();

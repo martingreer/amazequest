2cee6dd553faac394262c90130f821e1b798b88d
martingreer <greer@student.chalmers.se>
2015-01-30 11:24:21 +0100

    Added Hello World

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -2,7 +2,6 @@ import java.awt.FlowLayout;
 
 import javax.swing.*;
 
-@SuppressWarnings("serial")
 public class MainMenu extends JFrame {
 
 	public MainMenu() {
@@ -10,10 +9,14 @@ public class MainMenu extends JFrame {
 	}
 	
 	public final void initUI(){
-		setTitle("World of Swag");
+		setTitle("A Maze Quest");
 		setSize(700, 500);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public void HelloWorld(){
+		System.out.println("Hello world!");
 	}
 	
 	public static void main(String[] args) {

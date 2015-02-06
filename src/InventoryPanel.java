6b62501b656acc47297bfbc370789f4de7d32b31
JonasArvidsson1 <jonas.arvidsson0@gmail.com>
2015-02-06 12:12:27 +0100

    .

--- src/InventoryPanel.java
+++ src/InventoryPanel.java
@@ -3,6 +3,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;

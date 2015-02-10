252a342d038c35a34823bcf617a904bf03a38454
JonasArvidsson1 <jonas.arvidsson0@gmail.com>
2015-02-10 14:40:55 +0100

    Changed VERSION string

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -14,7 +14,7 @@ public class GameWindow {
 
 	private static final int WINDOW_SIZE_X = 464;
 	private static final int WINDOW_SIZE_Y = 610;
-	private static String VERSION = "A Maze Quest " + "\n" + "Version:" + " Alpha 13.37";
+	private static String VERSION = "A Maze Quest " + "\n" + "Version:" + " Alpha 13.37" + "\n" + "© Jonas Brothers";
 	private static GameFrame gameFrame;
 
 	public static void main(String[] args){

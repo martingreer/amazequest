c24ff96d09e529b876119565f2f5a0d12411338d
uhrj <uhr.johannes@gmail.com>
2015-02-12 14:42:36 +0100

    setplayer method

--- src/Tile.java
+++ src/Tile.java
@@ -3,16 +3,21 @@ public class Tile {
 	int xPos,yPos;
 	private String imgID;
 	private boolean collision;
+	private Player player;
 	
 	public Tile(int xPos, int yPos, String imgID, boolean collision){
 		this.xPos = xPos;
 		this.yPos = yPos;
 		this.imgID = imgID;
 		this.collision = collision;
+		
 	}
 	
 	public String getImgID(){
 		return imgID;
 	}
 	
+	public void setPlayer(Player player){
+		this.player = player;
+	}
 }

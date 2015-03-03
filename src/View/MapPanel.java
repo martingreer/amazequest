4189d7c72265f1028ee8f2329d1e2c7481d93968
JonasArvidsson1 <jonas.arvidsson0@gmail.com>
2015-03-03 15:35:54 +0100

    black FoW tile is default

--- src/View/MapPanel.java
+++ src/View/MapPanel.java
@@ -50,7 +50,7 @@ public class MapPanel extends JPanel {
 						g.drawImage(res.getImg(tiles[x][y].getInterObj().getName()), x*TILE_SIZE, y*TILE_SIZE, null);
 					}
 					if(tiles[x][y].isDark()){
-						g.drawImage(res.getImg("debugDark"), x*TILE_SIZE, y*TILE_SIZE, null);
+						g.drawImage(res.getImg("dark"), x*TILE_SIZE, y*TILE_SIZE, null);
 					}
 				}
 			}

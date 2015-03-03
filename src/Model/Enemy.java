0751947b73a1d91b666080e67d45c8099baafecc
uhrj <uhr.johannes@gmail.com>
2015-03-03 16:56:39 +0100

    added expreward to enemy

--- src/Model/Enemy.java
+++ src/Model/Enemy.java
@@ -3,12 +3,15 @@ package Model;
 
 public class Enemy extends InteractiveObject {
 
-	
+	private int expReward;
 	public Enemy(int level, int attack, int maxHp, int hp, String name) {
 		
 		super(level, attack, maxHp, hp, name);
-		
+		expReward = level * 10;
 	}
 	
+	public int getExpReward(){
+		return expReward;
+	}
 
 }
--- src/Model/GameLogic.java
+++ src/Model/GameLogic.java
@@ -211,7 +211,7 @@ public class GameLogic{
 		System.out.println("fighting");
 		player.exchangeHitsWithEnemy(nextTile);
 		if((nextTile.getInterObj().getHp()) <= 0 ){					//check enemy Hp, remove if <= 0.
-			player.setExp(nextTile.getInterObj().getLevel()*10); 	//player get experience,  enemylevel * 10
+			player.setExp(((Enemy)nextTile.getInterObj()).getExpReward()); 	//player get experience,  enemylevel * 10
 			nextTile.setEnemy(null);
 			nextTile.setBlood(true);
 			currentEnemy = null;

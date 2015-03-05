5e79362f7b154eff2b58aff9efb583f8e1d1b3c3
Johannes <Johannes@46.239.100.191>
2015-03-05 21:05:58 +0100

    javadoc for imageresources

--- src/Model/ImageResources.java
+++ src/Model/ImageResources.java
@@ -7,20 +7,37 @@ import java.util.Scanner;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
-
+/**
+ * This class is used to return requested images or image-paths to view-objects.
+ * 
+ * @author Johannes Uhr
+ * @version 2015-02-22
+ *
+ */
 public class ImageResources {
-
-	File file;
-
+	
+	/**
+	 * The file that holds all keywords and their paths
+	 */
+	private File file;
+	
+	/**
+	 * The constructor sets "file" to the txt-file that contains IDs and paths seperated by a ":".
+	 */
 	public ImageResources(){
 		file = new File("./res/ImagePaths.txt");
 	}
-
+	
+	/**
+	 * This method scans the txt-file for an ID and if found, returns the path connected to the ID.
+	 * 
+	 * @param String that resembles the keyword connected to a path.
+	 * @return String of the path to wanted image.
+	 */
 	public String getPath(String ID){
 		try {
 			Scanner scanner = new Scanner(file);
 
-			//now read the file line by line...
 			while (scanner.hasNextLine()) {
 				String line = scanner.nextLine();
 				String[] parts = line.split(":");
@@ -37,16 +54,23 @@ public class ImageResources {
 		return null;
 	}
 
+	/**
+	 * This method returns the Image of the path connected to the parameter.
+	 * 
+	 * @param String that resembles the keyword connected to a path.
+	 * @return Image that is connected to parameter.
+	 */
 	public Image getImg(String s){
 		ImageIcon imgIcon  = new ImageIcon(getPath(s));
 		Image img = imgIcon.getImage();
 		return img;
 	}
+
 	/**
-	 * Checks if there are no enemies the map, if so,
-	 * opens the door. 
+	 * This method returns the ImageIcon of the path connected to the parameter.
 	 * 
-	 * @return true if there is still an enemy on the map, otherwise false.
+	 * @param String that resembles the keyword connected to a path.
+	 * @return Image that is connected to parameter.
 	 */
 	public ImageIcon getImgIcon(String s){
 		return new ImageIcon(getPath(s));

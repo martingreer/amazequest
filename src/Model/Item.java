5d159128dcd7013d0fd3273c8bb3eb6cf8b3e429
JonasArvidsson1 <jonas.arvidsson0@gmail.com>
2015-03-06 00:22:42 +0100

    fix javadoc params

--- src/Model/Item.java
+++ src/Model/Item.java
@@ -1,6 +1,6 @@
 package Model;
 /**
- * This class is respnsible for all the item objects in the game. 
+ * This class is responsible for all the item objects in the game. 
  * Keeps track of all stats related to the item objects.  
  * @author Jonas Arvidsson
  * @version 2015-03-05
@@ -8,12 +8,11 @@ package Model;
 public class Item extends InteractiveObject{
 	/**
 	 * 
-	 * @param level	The level of the item
-	 * @param attack The attack of the item
-	 * @param maxHp The max health of the item	
-	 * @param hp The health of the item
-	 * @param name The name of the item
-	 * @param exp The experience of the item
+	 * @param level The level of the item
+	 * @param attack the attack of the item
+	 * @param maxHp the max health points of the item
+	 * @param hp the health points of the item
+	 * @param name the name of the item
 	 */
 	public Item(int level, int attack, int maxHp, int hp, String name) {
 		super(level, attack, maxHp, hp, name);
